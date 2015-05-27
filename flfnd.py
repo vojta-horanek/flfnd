a91e745747d28c88b1e3e59fbe6e5499ba04be41
vojta-horanek <vojtechhoranek@gmail.com>
2015-05-27 20:33:17 +0200

    Added sys.exit() for every function

--- flfnd.py
+++ flfnd.py
@@ -11,17 +11,21 @@ if len(sys.argv)<2:
    sys.exit()	
 if (os.path.isfile(sys.argv[1])):
 	print "File <%s> was found!" % sys.argv[1]
+	sys.exit()
 elif (sys.argv[1] == "--help"):
 	print "Add (only)one file argument to command"
 	print "--help                   print this screen"
 	print "--autor                  autor name and email adress"
 	print "--about                  about this program"
 	print "-c <filename>            create new file (based on touch command)"
+	sys.exit()
 elif (sys.argv[1] == "--about"):
 	print"Program to identify if the file exists"
 	print"Copyright Vojtech Horanek 2015"
+	sys.exit()
 elif (sys.argv[1] == "--autor"):
 	print"Vojtech Horanek <vojtechhoranek@gmail.com>"
+	sys.exit()
 elif (sys.argv[1] == "-c"):
 
 	if len(sys.argv)<3:
@@ -30,6 +34,8 @@ elif (sys.argv[1] == "-c"):
    	else:	
 		call("touch " + sys.argv[2], shell=True)
 		print "File %s was successfully created!" % sys.argv[2]
+		sys.exit()
 else:
 	print"File <%s> wasn't found!" % sys.argv[1]
 	print"If you want to create use <flfnd -c %s>" % sys.argv[1]
+	sys.exit()
