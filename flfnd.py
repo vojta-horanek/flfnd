a9f36f4a0e885606fde10dd74849dba2c55db5fb
vojta-horanek <vojtechhoranek@gmail.com>
2015-05-27 20:48:18 +0200

    Added verision command <--verision> and edited help

--- flfnd.py
+++ flfnd.py
@@ -8,6 +8,9 @@ import os.path
 import sys
 from subprocess import call
 
+def verision():
+	print"0.2"
+	sys.exit()
 if len(sys.argv)<2:
    print"You need to specify file to be find!" 
    sys.exit()	
@@ -20,17 +23,19 @@ elif (sys.argv[1] == "--help"):
 	print "--autor                  autor name and email adress"
 	print "--about                  about this program"
 	print "-c <filename>            create new file (based on touch command)"
+	print "--verision               print verision info"
 	sys.exit()
 elif (sys.argv[1] == "--about"):
 	print"Program to identify if the file exists"
 	print"Copyright Vojtech Horanek 2015"
-	printf "git-clone: https://github.com/vojta-horanek/flfnd.git " 
+	print"git: https://github.com/vojta-horanek/flfnd.git " 
 	sys.exit()
 elif (sys.argv[1] == "--autor"):
 	print"Vojtech Horanek <vojtechhoranek@gmail.com>"
 	sys.exit()
+elif (sys.argv[1] == "--verision"):
+        verision()
 elif (sys.argv[1] == "-c"):
-
 	if len(sys.argv)<3:
    		print"You need to specify file name to be created!" 
    		sys.exit()
